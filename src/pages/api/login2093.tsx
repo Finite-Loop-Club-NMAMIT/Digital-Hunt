6e14f880bd3dd2195c564395b799554115d70d1e
NidheeshaT <ntbayari@gmail.com>
2023-10-26 12:13:51 +0530

    feat: login page structure and inbox page gsh77s8

---
+++ src/pages/api/login2093.tsx
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req:NextApiRequest, res:NextApiResponse) {
+    //if password matches redirect to mailbox
+
+    res.redirect('/gsh77s8') 
+
+    //else error message to login page
+    res.redirect('/login2093?error=Wrong Password');
+}
--- src/pages/login2093.tsx
+++ src/pages/login2093.tsx
@@ -1,10 +1,93 @@
+import { useParams } from "next/navigation";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { number } from "zod";
+
+interface LoginPageContent {
+  loginTitle: string;
+  username: string;
+  password: string;
+  loginButton: string;
+  usernamePlaceholder: string;
+  passwordPlaceholder: string;
+}
+
 export default function Login() {
-    // get query paramater user , 
-    // if 'guest' show login page with password and username in the order
-    // whole page encrypted with ceaser cipher first with expect "Try different number of shifts"
-    // after correct ceaser cipher key everthing is correct expect the "Try different number of shifts"===encrypted password(play fair cipher))  
-    // and if 'admin' show login page with password and insolvable captcha
-    // cookies we hide playfair cipher key
-    // successfull login leads to mail page where we get list of mails
-    return <h1>Login</h1>
+  // get query paramater user ,
+  // if 'guest' show login page with password and username in the order
+  // whole page encrypted with ceaser cipher first with expect "Try different number of shifts"
+  // after correct ceaser cipher key everthing is correct expect the "Try different number of shifts"===encrypted password(play fair cipher))
+  // and if 'admin' show login page with password and insolvable captcha
+  // cookies we hide playfair cipher key
+  // successfull login leads to mail page where we get list of mails
+  const router = useRouter();
+  const { user } = router.query;
+  const [cipherKey, setCipherKey] = useState<number>(() => 0);
+  const [loginContent, setLoginContent] = useState<LoginPageContent>(() => ({
+    loginTitle: "Advxc",
+    username: "Jhtgcpbt",
+    password: "Ephhldgs",
+    loginButton: "Advxc",
+    usernamePlaceholder: "Tbpxa dg Ewdct",
+    passwordPlaceholder: "***********",
+  }));
+
+  const changeWords = (str: string, value: number) => {
+    let result = "";
+    for(const char of str){
+        if('A'.charCodeAt(0)<=char.charCodeAt(0) && char.charCodeAt(0)<='Z'.charCodeAt(0))
+            result+=String.fromCharCode(((char.charCodeAt(0)-'A'.charCodeAt(0)+value)%26)+'A'.charCodeAt(0));
+        else if('a'.charCodeAt(0)<=char.charCodeAt(0) && char.charCodeAt(0)<='z'.charCodeAt(0))
+            result+=String.fromCharCode(((char.charCodeAt(0)-'a'.charCodeAt(0)+value)%26)+'a'.charCodeAt(0));
+        else{
+            result+=char;
+        }
+    }
+    return result;
+  };
+
+  const changeCipher = (value: number) => {
+    const val=value-cipherKey+26;
+    console.log(val);
+    setCipherKey(() => value);
+    const p: LoginPageContent = {
+      loginTitle: changeWords(loginContent.loginTitle, val),
+      username: changeWords(loginContent.username, val),
+      password: changeWords(loginContent.password, val),
+      loginButton: changeWords(loginContent.loginButton, val),
+      usernamePlaceholder: changeWords(loginContent.usernamePlaceholder, val),
+      passwordPlaceholder: changeWords(loginContent.passwordPlaceholder, val),
+    };
+    setLoginContent(() => p);
+  };
+  return (
+    <main>
+      <h2>{loginContent.loginTitle}</h2>
+      <label htmlFor="cipher">Try different number of shifts</label>
+      <select
+        name="cipher"
+        value={cipherKey}
+        onChange={(e) => changeCipher(Number(e.target.value))}
+      >
+        {[...Array(26).keys()].map((i: number, key: number) => (
+          <option value={i} key={key}>
+            {i}
+          </option>
+        ))}
+      </select>
+
+      <form method="post" action="/api/login2093">
+        <label htmlFor="password">{loginContent.password}</label>
+        <input name="password" type="password" placeholder={loginContent.passwordPlaceholder} />
+        {user !== "admin" && (
+            <>
+            <label htmlFor="username">{loginContent.username}</label>
+            <input name="username" type="text" placeholder={loginContent.usernamePlaceholder} />
+
+            </>
+        )}
+        <button type="submit">{loginContent.loginButton}</button>
+      </form>
+    </main>
+  );
 }
