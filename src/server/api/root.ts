b5d4a707ed4e14f25063e4417f174656bc456633
SrivatsaRUpadhya <srivatsarupadhya@gmail.com>
2023-10-26 18:46:04 +0530

    get sorted submission list

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,12 +1,14 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { mailBoxRouter } from "./routers/mailbox";
+import { submissionsRouter } from "./routers/getSubmitions";
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-    mailBoxRouter  
+    mailBoxRouter,
+    submissionsRouter
 });
 
 // export type definition of API
--- src/server/api/routers/getSubmitions.ts
+++ src/server/api/routers/getSubmitions.ts
@@ -1,10 +1,47 @@
-import { publicProcedure,createTRPCRouter} from '../trpc';
+import { RoundOne, RoundTwo, User } from '@prisma/client';
+import { createTRPCRouter, protectedProcedure } from '../trpc';
+import z from "zod"
 
-export const mailBoxRouter = createTRPCRouter({
-  getAllMails: publicProcedure.query(async ({ctx,input})=>{
-    const p=await ctx.db.mailBox.findMany()
-    if(p===undefined)
-        throw new Error("no such mailbox")
-    return p
+type sortDataInput = {
+  id: string,
+  name: string | null,
+  points: number,
+  roundOne: { updatedAt: Date } | null
+  roundTwo: { updatedAt: Date } | null
+}
+const sort = (data: sortDataInput[], round: number) => {
+  if (round == 1) {
+    data.sort((a: sortDataInput, b: sortDataInput) => {
+      if (a.points == b.points) return Date.parse(`'${a.roundOne?.updatedAt}'`) - Date.parse(`'${b.roundOne?.updatedAt}'`);
+      return b.points - a.points;
+    })
+  }
+  else
+    data.sort((a: sortDataInput, b: sortDataInput) => {
+      if (a.points == b.points) return Date.parse((`'${a.roundTwo?.updatedAt}'`)) - Date.parse(`'${b.roundTwo?.updatedAt}'`);
+      return b.points - a.points;
+    })
+  return data
+}
+export const submissionsRouter= createTRPCRouter({
+  getSubmissions: protectedProcedure.query(async ({ ctx, input }) => {
+    const data = await ctx.db.user.findMany({
+      select: {
+        id: true,
+        name: true,
+        points: true,
+        roundOne: {
+          select: { updatedAt: true },
+        },
+        roundTwo: { select: { updatedAt: true } }
+      }
+    })
+
+    const roundOneSorted = sort(data, 1)
+    const roundTwoSorted = sort(data, 2)
+    return {
+      roundOneSorted,
+      roundTwoSorted
+    }
   })
 });
