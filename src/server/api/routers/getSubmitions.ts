d26a978a957ad6d54fa466881e67017098711fb7
SrivatsaRUpadhya <srivatsarupadhya@gmail.com>
2023-10-26 17:26:49 +0530

    create getSubmissions route

---
+++ src/server/api/routers/getSubmitions.ts
@@ -0,0 +1,10 @@
+import { publicProcedure,createTRPCRouter} from '../trpc';
+
+export const mailBoxRouter = createTRPCRouter({
+  getAllMails: publicProcedure.query(async ({ctx,input})=>{
+    const p=await ctx.db.mailBox.findMany()
+    if(p===undefined)
+        throw new Error("no such mailbox")
+    return p
+  })
+});
