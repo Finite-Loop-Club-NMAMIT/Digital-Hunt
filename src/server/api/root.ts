9f34db06f388db0d1c1e234822211e57a5331523
NidheeshaT <ntbayari@gmail.com>
2023-10-26 16:41:05 +0530

    feat: basic mailbox setup

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,11 +1,12 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-
+import { mailBoxRouter } from "./routers/mailbox";
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
+    mailBoxRouter  
 });
 
 // export type definition of API
