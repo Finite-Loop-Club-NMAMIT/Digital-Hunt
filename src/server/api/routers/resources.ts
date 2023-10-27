4d5f1921c3c1c7399f0f4890c20ce7397817e046
Srivatsa <srivatsarupadhya@gmail.com>
2023-10-27 23:54:41 +0530

    rearrange zip file ids

--- src/server/api/routers/resources.ts
+++ src/server/api/routers/resources.ts
@@ -5,13 +5,12 @@ export const files = createTRPCRouter({
 	getFile: protectedProcedure.query(async ({ ctx }) => {
 		try {
 			const { fileNumber, hash } = getFileNumber(ctx.session.user.id);
-			console.log(env.ZIP_FILES);
 			const fileLinks = {
-				"0": "1rUf5pCN-K-_v73Hev5ILjn59-IR90DMw",
-				"1": "1-S4ONWCzpyMJDyRz-H4E-9VBRR2gPSJH",
+				"0": "1JUMuMtqb-IwUKbjVnMiWaToMpPYx_Psg",
+				"1": "1C_OaonobyVeObKoeqmbaMlof2GhuCzAI",
 				"2": "1Ul3gROkOv6lclUCVRH5NS7cKftu0S3wX",
-				"3": "1C_OaonobyVeObKoeqmbaMlof2GhuCzAI",
-				"4": "1JUMuMtqb-IwUKbjVnMiWaToMpPYx_Psg",
+				"3": "1-S4ONWCzpyMJDyRz-H4E-9VBRR2gPSJH",
+				"4": "1rUf5pCN-K-_v73Hev5ILjn59-IR90DMw",
 			};
 			const downloadLink =
 				"https://drive.google.com/uc?export=download&id=" +
