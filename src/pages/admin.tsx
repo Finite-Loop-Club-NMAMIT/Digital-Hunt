35f88d9285401fede26188fe1800fcc93dde1515
Srivatsa <srivatsarupadhya@gmail.com>
2023-10-27 23:24:38 +0530

    restrict admin page to admins only

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -3,61 +3,117 @@ import { z } from "zod";
 import { api } from "~/utils/api";
 
 export default function Instagram() {
-    const { data: sessionData } = useSession();
-    const reduceScore = api.admin.reduceScore.useMutation()
-    const getSubmissions = api.admin.getSubmissions.useQuery().data
-    // function handleclick(e: Event) {
-    function handleclick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-        // reduceScore.mutate({ userid: z.string().parse(e.target.dataset.userid) })
-        const userId = (e.target as HTMLElement)?.dataset?.userid;
-        if (userId) 
-            reduceScore.mutate({ userid: z.string().parse(userId) });
-        else
-            console.error('User ID is null or undefined');
-    }
-    if (sessionData?.user) {
-        return (
-            <>
-            <h1 className="text-center text-2xl mt-20">All Submissions</h1>
-                <div className="w-fit m-auto">
-                    <h1 className="text-extrabold mt-10">Round 1</h1>
-                    <table className="border">
-                        <th className="p-3 border">UserID</th>
-                        <th className="p-3 border">Name</th>
-                        <th className="p-3 border">Points</th>
-                        <th className="p-3 border">Submited at</th>
-                        {getSubmissions?.roundOneSorted.map((element, key) => {
-                            return (
-                                <tr key={key}>
-                                    <td className="border p-5">{element.id}</td>
-                                    <td className="border p-5">{element.name}</td>
-                                    <td className="border p-5">{element.points1}</td>
-                                    <td className="border p-5">{element.roundOne?.createdAt.toDateString()}</td>
-                                    <td data-userid={element.id} className="border p-5"><button className="border rounded p-3 bg-black text-white" data-userid={element.id} onClick={(e) => handleclick(e)}>Reduce score by 10</button></td>
-                                </tr>
-                            )
-                        })}
-                    </table>
-                    <h1 className="text-extrabold mt-10">Round 2</h1>
-                    <table>
-                        <th className="p-3 border">UserID</th>
-                        <th className="p-3 border">Name</th>
-                        <th className="p-3 border">Points</th>
-                        <th className="p-3 border">Submited at</th>
-                        {getSubmissions?.roundTwoSorted.map((element, key) => {
-                            return element.roundTwo && (
-                                <tr key={key}>
-                                    <td className="border p-5">{element.id}</td>
-                                    <td className="border p-5">{element.name}</td>
-                                    <td className="border p-5">{element.points1}</td>
-                                    <td className="border p-5">{element.roundTwo?.createdAt.toDateString()}</td>
-                                    <td data-userid={element.id} className="border p-5"><button className="border rounded p-3 bg-black text-white" data-userid={element.id} onClick={(e) => handleclick(e)}>Reduce score by 10</button></td>
-                                </tr>
-                            )
-                        })}
-                    </table>
-                    </div>
-            </>
-        )
-    }
+	const { data: sessionData } = useSession();
+	const reduceScore = api.admin.reduceScore.useMutation();
+	const getSubmissions = api.admin.getSubmissions.useQuery();
+	// function handleclick(e: Event) {
+	function handleclick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+		// reduceScore.mutate({ userid: z.string().parse(e.target.dataset.userid) })
+		const userId = (e.target as HTMLElement)?.dataset?.userid;
+		if (userId) reduceScore.mutate({ userid: z.string().parse(userId) });
+		else console.error("User ID is null or undefined");
+	}
+	if (sessionData?.user) {
+		if (!getSubmissions.isLoading)
+			return (
+				<>
+					{getSubmissions?.data ? (
+						<div className="m-auto w-fit">
+							<h1 className="mt-20 text-center text-2xl">
+								All Submissions
+							</h1>
+							<h1 className="text-extrabold mt-10">Round 1</h1>
+							<table className="border">
+								<th className="border p-3">UserID</th>
+								<th className="border p-3">Name</th>
+								<th className="border p-3">Points</th>
+								<th className="border p-3">Submited at</th>
+								{getSubmissions?.data?.roundOneSorted.map(
+									(element, key) => {
+										return (
+											<tr key={key}>
+												<td className="border p-5">
+													{element.id}
+												</td>
+												<td className="border p-5">
+													{element.name}
+												</td>
+												<td className="border p-5">
+													{element.points1}
+												</td>
+												<td className="border p-5">
+													{element.roundOne?.createdAt.toDateString()}
+												</td>
+												<td
+													data-userid={element.id}
+													className="border p-5"
+												>
+													<button
+														className="rounded border bg-black p-3 text-white"
+														data-userid={element.id}
+														onClick={(e) =>
+															handleclick(e)
+														}
+													>
+														Reduce score by 10
+													</button>
+												</td>
+											</tr>
+										);
+									}
+								)}
+							</table>
+							<h1 className="text-extrabold mt-10">Round 2</h1>
+							<table>
+								<th className="border p-3">UserID</th>
+								<th className="border p-3">Name</th>
+								<th className="border p-3">Points</th>
+								<th className="border p-3">Submited at</th>
+								{getSubmissions?.data?.roundTwoSorted.map(
+									(element, key) => {
+										return (
+											element.roundTwo && (
+												<tr key={key}>
+													<td className="border p-5">
+														{element.id}
+													</td>
+													<td className="border p-5">
+														{element.name}
+													</td>
+													<td className="border p-5">
+														{element.points1}
+													</td>
+													<td className="border p-5">
+														{element.roundTwo?.createdAt.toDateString()}
+													</td>
+													<td
+														data-userid={element.id}
+														className="border p-5"
+													>
+														<button
+															className="rounded border bg-black p-3 text-white"
+															data-userid={
+																element.id
+															}
+															onClick={(e) =>
+																handleclick(e)
+															}
+														>
+															Reduce score by 10
+														</button>
+													</td>
+												</tr>
+											)
+										);
+									}
+								)}
+							</table>
+						</div>
+					) : (
+						<h2>{getSubmissions.error?.message}</h2>
+					)}
+				</>
+			);
+		return "Loading...";
+	}
 }
--- src/server/api/routers/admin.ts
+++ src/server/api/routers/admin.ts
@@ -1,73 +1,102 @@
-import { createTRPCRouter, protectedProcedure } from '../trpc';
+import { createTRPCRouter, protectedProcedure } from "../trpc";
 import z from "zod";
 
 type sortDataInput = {
-  id: string;
-  name: string | null;
-  points1: number; 
-  roundOne: { createdAt: Date } | null;
-  roundTwo: { createdAt: Date } | null;
+	id: string;
+	name: string | null;
+	points1: number;
+	roundOne: { createdAt: Date } | null;
+	roundTwo: { createdAt: Date } | null;
 };
 
 const sort = (data: sortDataInput[], round: number) => {
-  if (round === 1) {
-    data.sort((a: sortDataInput, b: sortDataInput) => {
-      const aCreatedAt = a.roundOne?.createdAt instanceof Date ? a.roundOne?.createdAt.toISOString() : '';
-      const bCreatedAt = b.roundOne?.createdAt instanceof Date ? b.roundOne?.createdAt.toISOString() : '';
-      if (a.points1 === b.points1) return Date.parse(aCreatedAt) - Date.parse(bCreatedAt);
-      return b.points1 - a.points1;
-    });
-  } else {
-    data.sort((a: sortDataInput, b: sortDataInput) => {
-      const aCreatedAt = a.roundTwo?.createdAt instanceof Date ? a.roundTwo?.createdAt.toISOString() : '';
-      const bCreatedAt = b.roundTwo?.createdAt instanceof Date ? b.roundTwo?.createdAt.toISOString() : '';
-      if (a.points1 === b.points1) return Date.parse(aCreatedAt) - Date.parse(bCreatedAt);
-      return b.points1 - a.points1;
-    });
-  }
-  return data;
+	if (round === 1) {
+		data.sort((a: sortDataInput, b: sortDataInput) => {
+			const aCreatedAt =
+				a.roundOne?.createdAt instanceof Date
+					? a.roundOne?.createdAt.toISOString()
+					: "";
+			const bCreatedAt =
+				b.roundOne?.createdAt instanceof Date
+					? b.roundOne?.createdAt.toISOString()
+					: "";
+			if (a.points1 === b.points1)
+				return Date.parse(aCreatedAt) - Date.parse(bCreatedAt);
+			return b.points1 - a.points1;
+		});
+	} else {
+		data.sort((a: sortDataInput, b: sortDataInput) => {
+			const aCreatedAt =
+				a.roundTwo?.createdAt instanceof Date
+					? a.roundTwo?.createdAt.toISOString()
+					: "";
+			const bCreatedAt =
+				b.roundTwo?.createdAt instanceof Date
+					? b.roundTwo?.createdAt.toISOString()
+					: "";
+			if (a.points1 === b.points1)
+				return Date.parse(aCreatedAt) - Date.parse(bCreatedAt);
+			return b.points1 - a.points1;
+		});
+	}
+	return data;
 };
 
 export const admin = createTRPCRouter({
-  getSubmissions: protectedProcedure.query(async ({ ctx }) => {
-    try {
-      const data = await ctx.db.user.findMany({
-        select: {
-          id: true,
-          name: true,
-          points1: true,
-          roundOne: {
-            select: { createdAt: true },
-          },
-          roundTwo: { select: { createdAt: true } }
-        }
-      });
+	getSubmissions: protectedProcedure.query(async ({ ctx }) => {
+		try {
+			const userId = ctx.session.user.id;
+			const user = await ctx.db.user.findUnique({
+				where: {
+					id: userId,
+				},
+			});
+			if (user?.role === "ORGANIZER") {
+				const data = await ctx.db.user.findMany({
+					select: {
+						id: true,
+						name: true,
+						points1: true,
+						roundOne: {
+							select: { createdAt: true },
+						},
+						roundTwo: { select: { createdAt: true } },
+					},
+				});
 
-      const roundOneSorted = sort(data, 1);
-      const roundTwoSorted = sort(data, 2);
-      return {
-        roundOneSorted,
-        roundTwoSorted
-      };
-    } catch (e) {
-      console.log(e);
-      throw new Error("An error occurred!");
-    }
-  }),
-  reduceScore: protectedProcedure.input(z.object({
-    userid: z.string()
-  })).mutation(async ({ ctx, input }) => {
-    try {
-      await ctx.db.user.update({
-        where: { id: input.userid },
-        data: {
-          points1: { decrement: 10 }
-        }
-      });
-      return { message: "success" };
-    } catch (e) {
-      console.log(e);
-      throw new Error("An error occurred!");
-    }
-  })
+				const roundOneSorted = sort(data, 1);
+				const roundTwoSorted = sort(data, 2);
+				return {
+					roundOneSorted,
+					roundTwoSorted,
+				};
+			} else {
+				throw {type:"Not permitted", message:"Oops! you cannot access this resource"};
+			}
+		} catch (e) {
+			console.log(e);
+			if(e.type === "Not permitted") throw e.message
+			throw new Error("An error occurred!");
+		}
+	}),
+	reduceScore: protectedProcedure
+		.input(
+			z.object({
+				userid: z.string(),
+			})
+		)
+		.mutation(async ({ ctx, input }) => {
+			try {
+				await ctx.db.user.update({
+					where: { id: input.userid },
+					data: {
+						points1: { decrement: 10 },
+					},
+				});
+				return { message: "success" };
+			} catch (e) {
+				console.log(e);
+				throw new Error("An error occurred!");
+			}
+		}),
 });
