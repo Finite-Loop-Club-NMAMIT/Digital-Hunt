bd8bd7763b5f058f0ec1e6d3c32ffbda88797dd6
Keerthan N S <keerthanns091@gmail.com>
2023-10-28 07:32:54 +0530

    [update]toLowerCase

--- src/pages/api/login2093.tsx
+++ src/pages/api/login2093.tsx
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 export default function handler(req:NextApiRequest, res:NextApiResponse) {
     //if password matches redirect to mailbox
     const {password}=req.body as {password:string}
-    if(password &&password==="cynthia")
+    if(password && password.toLowerCase()==="cynthia")
         res.redirect(301,'/gsh77s8') 
     else 
         res.redirect(301,"/login2093?user=guest&error=wrongpassword");
