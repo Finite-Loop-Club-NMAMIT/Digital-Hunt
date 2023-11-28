f17408b1a77a663a8a44dc02e17e3ef870ad6f52
Keerthan N S <keerthanns091@gmail.com>
2023-11-28 16:26:47 +0530

    [del]unused images

--- src/components/imageslider/index.tsx
+++ src/components/imageslider/index.tsx
@@ -9,8 +9,6 @@ const ImageSlider: FunctionComponent = () => {
   const [imagestate, setImagestate] = useState(["1.png", "2.png", "3.png", "4.png", "5.png",
                                                 "6.png", "7.png", "8.png", "9.png", "10.png",
                                                 "11.png", "12.png", "13.png", "14.png"]);
-                                                // "15.png", "16.png", "17.png", "18.png", "19.png", "20.png",
-                                                // "21.png", "22.png", "23.png"]);
 
   const handleNextSlide = () => {
     const newSlide = currentSlide === imagestate.length - 1 ? 0 : currentSlide + 1;
