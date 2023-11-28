8d2e605fc3bd969d843da9017a70e04b2b088c50
Keerthan N S <keerthanns091@gmail.com>
2023-11-28 22:52:48 +0530

    [update]round2 init page

--- src/components/imageslider/index.tsx
+++ src/components/imageslider/index.tsx
@@ -10,9 +10,6 @@ interface ImageSliderProps {
 const ImageSlider: FunctionComponent<ImageSliderProps> = ({ propsImages }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const [imagestate, setImageState] = useState<string[]>([]);
-  // const [imagestate, setImagestate] = useState(["1.png", "2.png", "3.png", "4.png", "5.png",
-  //                                               "6.png", "7.png", "8.png", "9.png", "10.png",
-  //                                               "11.png", "12.png", "13.png", "14.png"]);
   
   useEffect(() => {
     if (propsImages && propsImages.length > 0) {
--- src/pages/ZCisJ1BDKAaXtmJC.tsx
+++ src/pages/ZCisJ1BDKAaXtmJC.tsx
@@ -1,4 +1,5 @@
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import ImageSlider from "~/components/imageslider";
 import { api } from "~/utils/api";
@@ -43,10 +44,24 @@ export default function DownloadZip() {
 	else if (!getFile.error && getFile.data && typeof getFile.data === "string") {
       return (
         <>
-          <div className="bg-black min-h-screen flex flex-col items-center text-white px-3 py-4">
+          <div className="bg-black min-h-screen flex flex-col items-center text-center text-white px-3 py-4">
             <h1 className="text-lg font-semibold px-2">Congratulations!! on clearing Round1</h1>
-            <h1 className="text-3xl font-semibold neonText px-2">Round 2 Storyline</h1>
-            <ImageSlider propsImages={images}/>
+            <h1 className="text-3xl font-semibold neonText px-2 mb-3">Round 2 Storyline</h1>
+            <ImageSlider propsImages={images} />
+            <div className="mt-6 border border-green-400 flex flex-wrap gap-5 items-center justify-center p-3">
+              <Link className="flex-2 h-full text-white font-normal text-md bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br ring-green-900 ring-4 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-800/80 rounded-lg px-5 py-2.5 text-center"
+                  href={getFile.data.toString()} rel="noopener noreferrer" target="_blank">
+                  Download zip 📦
+              </Link>
+              <a className="flex-2 h-full text-white font-normal text-md bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br ring-green-900 ring-4 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-800/80 rounded-lg px-5 py-2.5 text-center"
+                  href="/bookmarks.txt" download="bookmarks.txt" rel="noopener noreferrer">
+                  Download bookmarks 📑
+              </a>
+              <Link className="flex-2 h-full text-white font-normal text-md bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br ring-green-900 ring-4 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-800/80 rounded-lg px-5 py-2.5 text-center"
+                  href={"/round2"} rel="noopener noreferrer" target="_blank">
+                  Round 2 Submission
+              </Link>
+            </div>
           </div>
         </>
       );
