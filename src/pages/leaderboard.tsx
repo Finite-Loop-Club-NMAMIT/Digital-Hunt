fb5e3f3cc1277d7408d8d3966c4a9c82b49da3bf
nagarajpandith <nagaraj.pandith2002@gmail.com>
2023-12-06 09:13:50 +0530

    fix: semantics

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -28,9 +28,9 @@ const Leaderboard = () => {
               <path
                 d="M21 12C21 16.9706 16.9706 21 12 21C9.69494 21 7.59227 20.1334 6 18.7083L3 16M3 12C3 7.02944 7.02944 3 12 3C14.3051 3 16.4077 3.86656 18 5.29168L21 8M3 21V16M3 16H8M21 3V8M21 8H16"
                 stroke="#000000"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
             </svg>
             {isRefetching ? "Loading..." : "Refresh"}
